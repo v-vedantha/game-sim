374b5d5fa33842c6473e22c1105f03e8c0a17c7a
Vedantha Venkatapathy <vedantha@Vedanthas-MacBook-Pro.local>
2024-12-28 17:42:52 -0800

    format CardParserTest.cpp

--- test/game/CardParserTest.cpp
+++ test/game/CardParserTest.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-#include "Card.h"
 #include "CardParser.h"
+#include "Card.h"
+#include <gtest/gtest.h>
 
 TEST(CardParsingTest, AceOfSpades) {
     EXPECT_EQ(parseCard("AS"), Card(Value::ACE, Suit::SPADES));
@@ -15,6 +15,7 @@ TEST(CardParsingTest, ElevenCausesException) {
 };
 
 TEST(CardParsingTest, MultipleCards) {
-    std::vector<Card> expectedOutput = {Card(Value::TEN, Suit::HEARTS), Card(Value::NINE, Suit::SPADES)};
+    std::vector<Card> expectedOutput = {Card(Value::TEN, Suit::HEARTS),
+                                        Card(Value::NINE, Suit::SPADES)};
     EXPECT_EQ(parseCards("10h9s"), expectedOutput);
 };
