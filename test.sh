5d15c617b05101640cc9e13a1d34005b5e25c719
Vedantha Venkatapathy <vedantha19@gmail.com>
2025-01-09 23:12:04 -0800

    build in parallel

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 set -e
-cmake --build build
-ctest --test-dir build  --timeout 4 --no-tests=error --output-on-failure
+cmake --build build -j8
+ctest --test-dir build -j8 --timeout 4 --no-tests=error --output-on-failure
